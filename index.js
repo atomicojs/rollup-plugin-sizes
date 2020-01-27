3c71c2b7441b6522766999f3df4c4a6f4f6ab411
UpperCod <mtrujillo@outlook.cl>
2020-01-27 12:47:09 -0300

    pkg update 1.1.0, thanks to @FezVrasta, log and test improvements are added

--- index.js
+++ index.js
@@ -7,69 +7,76 @@ var getBrotli = _interopDefault(require('brotli-size'));
 var table = _interopDefault(require('simple-string-table'));
 
 let color = {
-	reset: "\x1b[0m",
-	green: "\x1b[32m",
-	yellow: "\x1b[33m",
-	red: "\x1b[31m"
+  reset: "\x1b[0m",
+  green: "\x1b[32m",
+  yellow: "\x1b[33m",
+  red: "\x1b[31m"
 };
 
 function toKB(size) {
-	return (size / 1000).toFixed(2) + "KB";
+  return (size / 1000).toFixed(2) + "KB";
 }
 
-function sizes(limit) {
-	if (limit) {
-		let danger = Number(("" + limit).replace(/kb/gi, "")) * 1000;
-		limit = {
-			danger,
-			warning: danger * 0.9
-		};
-	}
-	function getColor(size) {
-		if (!limit) return color.green;
-		return size > limit.danger
-			? color.red
-			: size > limit.warning
-			? color.yellow
-			: color.green;
-	}
-	return {
-		name: "@atomico/rollup-plugin-sizes",
-		async writeBundle(bundles) {
-			let rows = [["FILES", "GZIP", "BROTLI"]];
-			let totalGzip = 0;
-			let totalBroli = 0;
-			let sizes = {};
-			let index = 1;
-			for (let key in bundles) {
-				let code = bundles[key].code;
-				if (typeof code !== "string") continue;
-				let gzip = await getGzip(code);
-				let brotli = await getBrotli(code);
-				totalGzip += gzip;
-				totalBroli += brotli;
-				sizes[index++] = gzip;
-				rows.push([key, toKB(gzip), toKB(brotli)]);
-			}
+function sizes(limit, customLog) {
+  if (limit) {
+    let danger = Number(("" + limit).replace(/kb/gi, "")) * 1000;
+    limit = {
+      danger,
+      warning: danger * 0.9
+    };
+  }
+  function getColor(size) {
+    if (!limit) return color.green;
+    return size > limit.danger
+      ? color.red
+      : size > limit.warning
+      ? color.yellow
+      : color.green;
+  }
+  return {
+    name: "@atomico/rollup-plugin-sizes",
+    async writeBundle(bundles) {
+      let rows = [[bundles.length > 1 ? "FILES" : "FILE", "GZIP", "BROTLI"]];
+      let totalGzip = 0;
+      let totalBroli = 0;
+      let sizes = {};
+      let index = 1;
+      for (let key in bundles) {
+        let code = bundles[key].code;
+        if (typeof code !== "string") continue;
+        let gzip = await getGzip(code);
+        let brotli = await getBrotli(code);
+        totalGzip += gzip;
+        totalBroli += brotli;
+        sizes[index++] = gzip;
+        rows.push([key, toKB(gzip), toKB(brotli)]);
+      }
 
-			rows.push(["", toKB(totalGzip), toKB(totalBroli)]);
-			sizes[index] = totalGzip;
+      if (bundles.length > 1) {
+        rows.push(["", toKB(totalGzip), toKB(totalBroli)]);
+      }
 
-			console.log(
-				[color.reset]
-					.concat(
-						table(rows)
-							.split("\n")
-							.map(
-								(stringRow, index) =>
-									(sizes[index] ? getColor(sizes[index]) : "") + stringRow
-							),
-						color.reset
-					)
-					.join("\n")
-			);
-		}
-	};
+      sizes[index] = totalGzip;
+
+      if (customLog) {
+        customLog(rows);
+      } else {
+        console.log(
+          [color.reset]
+            .concat(
+              table(rows)
+                .split("\n")
+                .map(
+                  (stringRow, index) =>
+                    (sizes[index] ? getColor(sizes[index]) : "") + stringRow
+                ),
+              color.reset
+            )
+            .join("\n")
+        );
+      }
+    }
+  };
 }
 
 module.exports = sizes;
